7ea1efa4a0a5bf0027a80829045982c3f51fcf62
LucaDangeloS <lucad7@gmail.com>
2023-07-30 02:06:26 +0200

    Updated remainingTime negative values and added method to get remaining time in string

--- app/src/main/java/com/example/coruabuswear/data/models/Bus.kt
+++ app/src/main/java/com/example/coruabuswear/data/models/Bus.kt
@@ -1,20 +1,46 @@
 package com.example.coruabuswear.data.models
 
 data class Bus(val id: Int, val line: BusLine) {
+    /*
+        * -3: Not initialized
+        * -2: 0 min
+        * -1: <1 min
+        * 0 = -2 (for sorting purposes)
+     */
     var remainingTime: Int
-
     fun updateRemainingTime(remainingTime: Int) {
-        this.remainingTime = remainingTime
+        if (remainingTime == 0) {
+            this.remainingTime = -2
+        } else {
+            this.remainingTime = remainingTime
+        }
     }
     constructor(id: Int, line: BusLine, remainingTime: Int) : this(id, line) {
-        this.remainingTime = remainingTime
+        if (remainingTime == 0) {
+            this.remainingTime = -2
+        } else {
+            this.remainingTime = remainingTime
+        }
     }
     init {
-        this.remainingTime = -2
+        this.remainingTime = -3
     }
 
     @Override
     override fun toString(): String {
         return "Bus(id=$id, line=$line, remainingTime=$remainingTime)"
+    }
+
+    fun getRemainingTime(): String {
+        return if (remainingTime == -2) {
+            "N/A"
+        } else if (remainingTime == -1) {
+            "<1 min"
+        } else if (remainingTime == 0) {
+            // TODO I18N
+            "En parada"
+        } else {
+            "$remainingTime min"
+        }
     }
 }
