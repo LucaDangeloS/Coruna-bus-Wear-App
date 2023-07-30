d546286b10ecf2ffaef243822849eec000c05e2f
LucaDangeloS <lucad7@gmail.com>
2023-07-30 15:24:30 +0200

    Added Utils for Dp Px conversions

---
+++ app/src/main/java/com/example/coruabuswear/Utils.kt
@@ -0,0 +1,8 @@
+package com.example.coruabuswear
+
+import android.content.res.Resources
+import androidx.compose.ui.unit.Dp
+
+fun Int.toDp(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+fun Dp.toPx(): Int = (this.value * Resources.getSystem().displayMetrics.density).toInt()
