a5cbbaf8210c7b4eacb0e35e0b22816791b0634a
LucaDangeloS <lucad7@gmail.com>
2024-07-20 13:53:39 +0200

    Added spacings in wear app constants for clarity

--- app/src/main/java/com/ldangelo/corunabuswear/data/Constants.kt
+++ app/src/main/java/com/ldangelo/corunabuswear/data/Constants.kt
@@ -10,14 +10,19 @@ object AppConstants {
     const val DEBUG = true
     const val LOG_FILE = "log_"
     const val SETTINGS_PREF = "settings"
+
     const val STOPS_FETCH_KEY = "stops_fetch"
     const val DEFAULT_STOPS_FETCH = 5
+
     const val STOPS_RADIUS_KEY = "stops_radius"
     const val DEFAULT_STOPS_RADIUS = 1000
+
     const val LOC_DISTANCE_KEY = "location_distance"
     const val DEFAULT_LOC_DISTANCE = 30f
+
     const val LOC_INTERVAL_KEY = "location_interval"
     const val DEFAULT_LOC_INTERVAL = 30000L
+
     const val FETCH_ALL_BUSES_ON_LOCATION_UPDATE_KEY = "fetch_all_buses_on_location_update"
     const val DEFAULT_FETCH_ALL_BUSES_ON_LOCATION_UPDATE = true
 }
