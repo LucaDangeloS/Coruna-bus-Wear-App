616e4393ac7d6d35044047308bd775ccac35a5bb
LucaDangeloS <lucad7@gmail.com>
2024-07-20 15:00:24 +0200

    [COMPANION] Added paths for Wear and companion app communication

---
+++ corunabuscompanion/src/main/java/com/ldangelo/corunabuswear/data/Paths.kt
@@ -0,0 +1,13 @@
+package com.ldangelo.corunabuswear.data
+
+object MessagePaths {
+    object IN {
+        const val DEPLOY_SETTINGS = "/deploy/settings"
+    }
+    object OUT {
+        const val DEPLOY = "/deploy"
+        const val SET_SETTINGS = "/settings/set"
+    }
+    const val GET_SETTINGS = "/settings/get"
+    const val SETTINGS = "/settings"
+}
