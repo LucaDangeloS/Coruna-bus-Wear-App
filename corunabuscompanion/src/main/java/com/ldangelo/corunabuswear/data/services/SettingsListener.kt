720fd2bf0663f40de5bdaee6202f4c0fe4bb835b
LucaDangeloS <lucad7@gmail.com>
2024-07-20 15:01:34 +0200

    [COMPANION] Refactored SettingsListener service

--- corunabuscompanion/src/main/java/com/ldangelo/corunabuswear/data/services/SettingsListener.kt
+++ corunabuscompanion/src/main/java/com/ldangelo/corunabuswear/data/services/SettingsListener.kt
@@ -1,24 +1,20 @@
 package com.ldangelo.corunabuswear.data.services
 
 import android.content.Intent
-import android.util.Log
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.WearableListenerService
-import com.ldangelo.corunabuswear.SyncActivity
+import com.ldangelo.corunabuswear.activities.SettingsActivity
+import com.ldangelo.corunabuswear.data.MessagePaths.IN.DEPLOY_SETTINGS
 
 class SettingsListener: WearableListenerService() {
 
     override fun onMessageReceived(messageEvent: MessageEvent) {
-//        Log.d(TAG, String(messageEvent.data))
-        if (messageEvent.path == MESSAGE_PATH) {
-            val startIntent = Intent(this, SyncActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        when (messageEvent.path) {
+            DEPLOY_SETTINGS -> {
+                startActivity(Intent(this, SettingsActivity::class.java).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                })
             }
-            startActivity(startIntent)
         }
-    }
-
-    companion object{
-        private const val MESSAGE_PATH = "/deploy/settings"
     }
 }
